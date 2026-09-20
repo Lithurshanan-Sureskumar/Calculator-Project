ef17a968e2381a720bafe964c2d90d8ffbf86aca
Vraj <home@Vrajs-MacBook-Pro.local>
2026-09-19 22:01:22 -0400

    Added Loop

--- calculator.py
+++ calculator.py
@@ -1,1 +1,35 @@
-# Test
+"""
+PROGRAM: Basic Calculator
+AUTHORS: Vraj Patel, Lithurshanan Sureskumar
+DESCRIPTION: 1st Collobaorative Project, Basic Calculator Application
+"""
+
+#Title
+print("------ CALCULATOR -----")
+
+# User's Input
+Calculator_Function = int(input("Please select what math operation you would like to use: \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Divison\n Enter Option (1-4):"))
+
+while Calculator_Function < 1 or Calculator_Function > 4:
+    print("Invalid selection. Please try again")
+    Calculator_Function = int(input("Please select what math operation you would like to use: \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Divison\n Enter Option (1-4): "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
